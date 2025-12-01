6cca57d169966a31504fd32c14132f0479212253
teemu <poutanenteemu1@gmail.com>
2025-12-01 14:10:23 +0200

    PDF luetaan ja palautetaan lista jonka alkiot ovat kysymyksiä

--- src/pdf_to_chunks.py
+++ src/pdf_to_chunks.py
@@ -2,18 +2,29 @@ import pdfplumber
 import ollama
 import chromadb
 import re
+import os.path
 
 
-def pdf_to_text():
-    with pdfplumber.open("./data/Kysymykset.pdf") as pdf:
-        text = ""
-        for page in pdf.pages:
-            text += page.extract_text()
+# Luetaan PDF tiedosto ja palautetaan se listana jossa kysymys per alkio
+def pdf_to_text(file_path: str):
+    if os.path.exists(file_path):
+        with pdfplumber.open(file_path) as pdf:
+            text_content = ""
+            for page in pdf.pages:
+                text_content += page.extract_text()
 
-        text = re.sub(r"\(cid:\d+\)", "", text)
-        text = re.sub(r"\[[^\]]*\]", "", text)
+            text_content = re.sub(r"\(cid:\d+\)", "", text_content)
+            text_content = re.sub(r"\[[^\]]*\]", "", text_content)
 
-        print(text.split(""))
+            chunks = re.findall(r"\d+\.\s.*?(?=\d+\.|$)", text_content, re.DOTALL)
+            chunks = [osa.replace("\n", " ").strip() for osa in chunks]
 
+            return chunks
+    else:
+        print("Path doesn't exist!")
+        return None
 
-pdf_to_text()
+path = "./data/Kysymykset.pdf"
+chunks = pdf_to_text(path)
+def chunks_to_embeddings(text_cbunks: list):
+
