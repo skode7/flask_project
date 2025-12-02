e3f4d927bb01e88966b11794431028df4dc47ac2
skode7 <poutanenteemu1@gmail.com>
2025-12-02 19:57:13 +0200

    luotu filet

--- src/code_explainer.py
+++ src/code_explainer.py
@@ -1,5 +1,28 @@
 from ollama import chat
+from ollama import ChatResponse
+from flask import Flask
 
-model = chat(model="poro-34b-chat")
-response = model.chat("Selitä tämä Python-koodi suomeksi:\n\nfor i in range(5): print(i)")
-print(response)
+
+def prompt_to_llm(prompt: str) -> str:
+
+    response: ChatResponse = chat(
+        model="jobautomation/OpenEuroLLM-Finnish",
+        messages= [
+            {
+                "role": "system",
+                "content": (
+                    f"Toimit koodausmentorina sovelluksessa, jossa käyttäjät esittävät ohjelmointiin liittyviä kysymyksiä. "
+                    f"Tavoitteesi on auttaa käyttäjiä oppimaan, ei vain ratkaisemaan ongelmia. "
+                    f"Älä anna suoraa ratkaisua tai kirjoita valmista koodia ellei käyttäjä nimenomaisesti pyydä sitä. "
+                    f"Sen sijaan tue käyttäjän ajattelua kysymällä tarkentavia kysymyksiä, esittämällä vihjeitä ja avaamalla ongelman taustoja. "
+                    f"Selitä tarvittaessa käsitteitä ja ehdota, miten käyttäjä voisi lähestyä ongelmaa. "
+                    f"Kannusta käyttäjää tutkimaan ja kokeilemaan ratkaisuja itse, sekä tarjoa tarvittaessa lisätukea ja rohkaisua. Ole aina ystävällinen ja kärsivällinen"
+                )
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ])
+
+    return response["message"]["content"]
