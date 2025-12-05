ba4f1f460595683836c4222e39fd5121dd6a24d5
skode7 <poutanenteemu1@gmail.com>
2025-12-05 14:42:14 +0200

    lisätty async funktio llm kyselyä varten

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,2 +1,24 @@
-const input = document.getElementById("input")
-const submit = document.createElement("submit")
+const chatDiv = document.getElementById("chatDiv");
+const submitBtn = document.getElementById("submit-btn");
+const span = document.createElement("span");
+const msgDiv = document.getElementById("messages")
+
+async function fetchData (input) {
+    const url = "http://127.0.0.1:5000/ask/" + input
+    const response = await fetch(url)
+    return await response.json()
+}
+
+async function showMessage(message) {
+    span.textContent = message["reply"]
+    msgDiv.append(span)
+
+}
+
+submitBtn.addEventListener("click", async (evt) => {
+    const inputValue = document.getElementById("txtarea").value;
+    const data = await fetchData(inputValue)
+    evt.preventDefault()
+    await showMessage(data)
+
+})
